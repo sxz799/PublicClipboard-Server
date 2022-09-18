b73e2b06081beaaf412fb7e471192a2f24dabba7
sxz799 <sxz15371@163.com>
2022-09-18 17:02:13 +0800

    配置gin发行版

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"PublicClipboard/router"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
 	r := gin.Default()
+	gin.SetMode(gin.ReleaseMode)
 	router.RegRouter(r)
 	r.Run(":9090")
 }
